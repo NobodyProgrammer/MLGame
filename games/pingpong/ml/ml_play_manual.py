9b35010fff6d655d418627112fb94f113c151c95
zzzz20102xx <zz20102aa@gmail.com>
2021-03-18 00:23:06 +0800

    [New] predict the drop point but it's accurate

--- games/pingpong/ml/ml_play_manual.py
+++ games/pingpong/ml/ml_play_manual.py
@@ -4,11 +4,12 @@ The script that send the instruction according to the keyboard input
 
 import pygame
 
+
 class MLPlay:
     def __init__(self, side):
         self._pygame_init()
         print("Invisible joystick is used for the {} side."
-            .format(side))
+              .format(side))
 
     def _pygame_init(self):
         pygame.display.init()
@@ -18,7 +19,6 @@ class MLPlay:
     def update(self, scene_info):
         if scene_info["status"] != "GAME_ALIVE":
             return "RESET"
-
         pygame.event.pump()
         key_pressed_list = pygame.key.get_pressed()
         if key_pressed_list[pygame.K_LEFT]:
